13af4fa1c476869ce7d8135815df49e1dded0d93
davis8988 <davismarsel@gmail.com>
2019-12-27 13:31:29 +0200

    Updated start printing coloring and text

--- start_smart_home.sh
+++ start_smart_home.sh
@@ -1,14 +1,17 @@
+RED='\033[0;31m'
+BLUE='\033[0;34m'
 CYAN='\033[0;36m'
 ORANGE='\033[0;33m'
 NC='\033[0m' # No Color
 
-echo -e "\n      /\\"
-echo -e "\n    /    \\"
-echo -e "\n  /        \\"
-echo -e "\n/            \\"
-echo -e "\n==============="
-echo -e "\n= ${CYAN}Smart Home${NC} ="
-echo -e "\n${ORANGE}==============="
-echo -e "\n${ORANGE}===============\n"
-echo -e "Starting.."
-# docker-compose up -d
+echo -e "\n${RED}      /\\"
+echo -e "    /  / \\"
+echo -e "  /  /   / \\"
+echo -e "/   /  /  / \\"
+echo -e "${BLUE}=============="
+echo -e "${BLUE}=            ="
+echo -e "= ${CYAN}Smart Home${BLUE} ="
+echo -e "${ORANGE}=            ="
+echo -e "==============\n${NC}"
+
+docker-compose up -d
