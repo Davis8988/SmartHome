3db6f43ec21dd75df47a8aced929805fd2f6d276
davis8988 <Davismarsel@gmail.com>
2019-12-27 13:20:30 +0200

    Added start smart home stack script

---
+++ start_smart_home.sh
@@ -0,0 +1,14 @@
+CYAN='\033[0;36m'
+ORANGE='\033[0;33m'
+NC='\033[0m' # No Color
+
+echo -e "\n      /\\"
+echo -e "\n    /    \\"
+echo -e "\n  /        \\"
+echo -e "\n/            \\"
+echo -e "\n==============="
+echo -e "\n= ${CYAN}Smart Home${NC} ="
+echo -e "\n${ORANGE}==============="
+echo -e "\n${ORANGE}===============\n"
+echo -e "Starting.."
+# docker-compose up -d
