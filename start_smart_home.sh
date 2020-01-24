71bb6f946a5dc80073ad3fc81d37efc62a31146e
davis8988 <Davismarsel@gmail.com>
2020-01-24 15:14:32 +0200

    Improved title

--- start_smart_home.sh
+++ start_smart_home.sh
@@ -1,18 +1,21 @@
-RED='\033[0;31m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
+RED='\033[1;31m'
+BLUE='\033[1;34m'
+CYAN='\033[1;36m'
 ORANGE='\033[0;33m'
+GRAY='\033[0;37m'
 NC='\033[0m' # No Color
 
-echo -e "\n${RED}      /\\"
-echo -e "    /  / \\"
-echo -e "  /  /   / \\"
-echo -e "/   /  /  / \\"
-echo -e "${BLUE}=============="
-echo -e "${BLUE}=            ="
-echo -e "= ${CYAN}Smart Home${BLUE} ="
-echo -e "${ORANGE}=            ="
-echo -e "==============\n${NC}"
+echo -e " \n${RED}"
+echo -e "        .    ${GRAY}~~~~~${RED}"
+echo -e "      ./ \\. ||"
+echo -e "    ./ ./  \\||"
+echo -e "  ./ ./  ./ ||"
+echo -e " /  /   /  /  \\"
+echo -e "${BLUE}================"
+echo -e "${BLUE}=              ="
+echo -e "=  ${CYAN}Smart Home${BLUE}  ="
+echo -e "${ORANGE}=              ="
+echo -e "================\n${NC}"
 
 
 echo Changing permissions
